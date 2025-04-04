82b464f2bd125db458daf60e216d50dfe678dc73
Ana Julia <anajuliadefendi@gmail.com>
2025-04-04 14:57:18 -0300

    tentando adicionar cores

--- src/pages/Consulta.jsx
+++ src/pages/Consulta.jsx
@@ -1,14 +1,15 @@
 function Consulta(){
     return(
         <div>
-            <div class='flex'>
-                <div class='bg-amber-100 w-10/12 h-full'>
+            <div className = 'flex'>
+                <div className = 'flex-col bg-fundo-marrom w-2/12 h-full'>
+                    <h1>y</h1>
+                </div>
+
+                <div className = 'flex-col-reverse bg-amber-100 w-10/12 h-full'>
                     <h1>x</h1>
                 </div>
                 
-                <div>
-
-                </div>
             </div>
         </div>
     )
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,26 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins :'Poppins'
+      },
+      colors: {
+        'texto-marrom': '#331a08',
+        'fundo-marrom': '#5b3011',
+        'botao-marrom': '#9f6d3d',
+        'login-marrom': '#ad795b',
+        'botao-bege': '#dbd0b3',
+        'fundo-bege': '#f0e7c2',
+        'botao-cinza': '#848484'
+      }
+    }
+  },
   plugins: [
     react(),
     tailwindcss(),
