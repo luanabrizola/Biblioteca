27892947a4c7d5ac9774e71d990c5f1055aeb16f
Ana Julia <anajuliadefendi@gmail.com>
2025-04-07 11:08:07 -0300

    Update Consulta.jsx

--- src/pages/Consulta.jsx
+++ src/pages/Consulta.jsx
@@ -6,12 +6,20 @@ function Consulta(){
                 </div>
 
                 <div className = 'flex-col-reverse bg-[#f0e7c2] w-10/12 h-full'>
-                    <div>
-                        <input 
-                            className="bg-[#956f54] rounded-xl w-7/12 h-10 placeholder:text-[#5b3011] placeholder:pl-4" 
-                            type="text" 
-                            placeholder="Digite sua busca"
-                        />
+                    <div className="mt-12">
+                        <form action="get">
+                            <input 
+                                className="bg-[#956f54] rounded-xl w-7/12 h-10 ml-4 placeholder:text-[#5b3011] placeholder:pl-4" 
+                                type="text" 
+                                placeholder="Digite sua busca"
+                            />
+                            <select name="select" id="0" className="bg-[#956f54] rounded-xl h-10 ml-4 w-40">
+                                <option value="0">Busca livre</option>
+                            </select>
+                            <button className="bg-[#956f54] rounded-[50%] h-10 w-10 ml-4">
+                                x
+                            </button>
+                        </form>
                     </div>
                 </div>
                 
