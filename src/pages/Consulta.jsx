ce47c9bba5cf5d4127cc417e0142fd0d9fec0277
Ana Julia <anajuliadefendi@gmail.com>
2025-04-10 08:14:25 -0300

    página de login

--- src/pages/Consulta.jsx
+++ src/pages/Consulta.jsx
@@ -1,37 +1,34 @@
-import { IconSearch } from '@tabler/icons-react';
+// import { IconSearch } from '@tabler/icons-react';
 
 function Consulta(){
     return(
         <div className = 'flex w-full h-full'>
-            <div className = 'flex w-full h-full'>
-                <div className = 'w-2/12 h-full bg-[#5b3011] flex flex-col justify-between'>
-                    <button className='bg-[#dbd0b3] w-full h-10 rounded-e-xl font-bold text-white mt-48'>Consulta</button>
-                    <button className='bg-[#9f6d3d] w-full h-10 font-bold text-white'>Entrar</button>
-                </div>
+            <div className = 'w-2/12 h-full bg-[#5b3011] flex flex-col justify-between'>
+                <button className='bg-[#dbd0b3] w-full h-10 rounded-e-xl font-bold text-white mt-48'>Consulta</button>
+                <button className='bg-[#9f6d3d] w-full h-10 font-bold text-white'>Entrar</button>
+            </div>
 
-                <div className = 'bg-[#f0e7c2] w-10/12 h-full'>
-                    <div className="mt-12">
-                        <form action="get" className="flex items-center justify-center gap-4 mt-12 ml-4 flex-wrap">
-                            <input 
-                                className="bg-[#956f54] rounded-xl w-7/12 h-10 placeholder:text-[#5b3011]" 
-                                type="text" 
-                                placeholder="Digite sua busca"
-                            />
-                            <select name="select" id="0" className="bg-[#956f54] text-white rounded-xl h-10 w-40 font-poppins">
-                                <option value="0">Busca livre</option>
-                                <option value="1">Título</option>
-                                <option value="2">Autor</option>
-                                <option value="3">Editora</option>
-                                <option value="4">Categoria</option>
-                                <option value="5">Subcategoria</option>
-                            </select>
-                            <button className="bg-[#956f54] text-white rounded-full h-10 w-10 flex items-center justify-center">
-                                <IconSearch></IconSearch>
-                            </button>
-                        </form>
-                    </div>
+            <div className = 'bg-[#f0e7c2] w-10/12 h-full'>
+                <div className="mt-12">
+                    <form action="get" className="flex items-center justify-center gap-4 mt-12 ml-4 flex-wrap">
+                        <input 
+                            className="bg-[#956f54] rounded-xl w-7/12 h-10 placeholder:text-[#5b3011]" 
+                            type="text" 
+                            placeholder="Digite sua busca"
+                        />
+                        <select name="select" id="0" className="bg-[#956f54] text-white rounded-xl h-10 w-40 font-poppins">
+                            <option value="0">Busca livre</option>
+                            <option value="1">Título</option>
+                            <option value="2">Autor</option>
+                            <option value="3">Editora</option>
+                            <option value="4">Categoria</option>
+                            <option value="5">Subcategoria</option>
+                        </select>
+                        <button className="bg-[#956f54] text-white rounded-full h-10 w-10 flex items-center justify-center">
+                            {/* <IconSearch></IconSearch> */}
+                        </button>
+                    </form>
                 </div>
-                
             </div>
         </div>
     )
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,18 @@
 function Login(){
     return(
-        <div>
-            <h1>
-                Login
-            </h1>
+        <div className="flex w-full h-full justify-center items-center bg-[url(./src/img/width_800.jpeg)] bg-cover">
+            <div className="flex flex-col justify-center items-center w-1/3 h-1/3 bg-white opacity-20 rounded-md">
+                <div>
+                    <p>Login</p>
+                    <input type="text" />
+                </div>
+                <div>
+                    <p>Senha</p>
+                    <input type="text" placeholder="Digite sua senha"/>
+                </div>
+                <a href="">Esqueceu a senha?</a>  
+                <button className="bg-[#ad795b] text-white font-bold rounded-md h-10 w-40">Entrar</button>
+            </div>
         </div>
     )
 }
