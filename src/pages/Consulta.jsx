fe5539ffcbd3fa8a333da6f2cf9422262c5a2bb2
Ana Julia <anajuliadefendi@gmail.com>
2025-04-18 14:53:16 -0300

    ajustando pagina de consulta

--- src/pages/Consulta.jsx
+++ src/pages/Consulta.jsx
@@ -11,14 +11,14 @@ function Consulta() {
         <div className='flex w-full h-full font-poppins'>
 
             <div className='bg-[#f0e7c2] w-full h-full font-poppins'>
-                <div className="mt-12">
-                    <form action="get" className="flex items-center justify-center gap-4 mt-12 ml-4 flex-wrap">
+                <div className="mt-12 mb-20">
+                    <form action="get" className="flex items-center justify-center gap-4 mt-12 flex-wrap">
                         <input
-                            className="bg-[#5b3011]/48 rounded-xl w-7/12 h-10 placeholder:text-[#5b3011]/44 px-3"
+                            className="bg-[#5b3011]/48 rounded-xl w-[60%] h-10 placeholder:text-[#5b3011]/44 px-3"
                             type="text"
                             placeholder="O diário de Anne Frank"
                         />
-                        <select name="select" id="0" className="bg-[#5b3011]/48 text-white rounded-xl h-10 w-40 font-poppins px-3">
+                        <select name="select" id="0" className="bg-[#5b3011]/48 text-white rounded-xl h-10 w-40 font-poppins px-3 cursor-pointer">
                             <option value="0">Busca livre</option>
                             <option value="1">Título</option>
                             <option value="2">Autor</option>
@@ -26,17 +26,17 @@ function Consulta() {
                             <option value="4">Categoria</option>
                             <option value="5">Subcategoria</option>
                         </select>
-                        <button className="bg-[#5b3011]/48 text-white rounded-full h-10 w-10 flex items-center justify-center">
+                        <button className="bg-[#5b3011]/48 text-white rounded-full h-10 w-10 flex items-center justify-center cursor-pointer">
                             <span className="material-icons">search</span>
                         </button>
                     </form>
                 </div>
 
-                <div className="flex flex-col items-center w-[90%] h-full mt-14">
-                    <div className="flex bg-white w-[90%] h-4/12 mb-5 rounded-md">
-                        <img src="./src/img/DiarioAnneFrank.png" className="w-45 h-[90%] flex self-center ml-4" />
-                        <div className="ml-5 h-[90%] mt-2">
-                            <h1 className="text-xl font-bold">O Diário de Anne Frank </h1>
+                <div className="flex flex-col items-center w-full gap-5 mt-4 mb-10">
+                    <div className="flex bg-white w-[75%] h-4/12 mb-5 rounded-md">
+                        <img src="./src/img/DiarioAnneFrank.png" className="w-45 h-70 flex self-center ml-4 mt-4 mb-4" />
+                        <div className="ml-5 h-[90%] mt-4">
+                            <h1 className="text-xl font-bold mb-2">O Diário de Anne Frank </h1>
                             <p>Autores: Anne Frank</p>
                             <p>Edição: Anne 99°</p>
                             <p>Editora: Record</p>
@@ -51,17 +51,17 @@ function Consulta() {
 
                             <button
                                 onClick={() => setVerMais1(!verMais1)}
-                                className="text-black underline font-bold mt-2"
+                                className="text-black underline font-bold mt-2 cursor-pointer"
                             >
                                 {verMais1 ? "Ver menos" : "Ver mais"}
                             </button>
                         </div>
 
                     </div>
-                    <div className="flex bg-white w-[90%] h-4/12 mb-5 rounded-md">
-                        <img src="./src/img/AnneFrankRed.png" className="w-45 h-[90%] flex self-center ml-4" alt="" />
-                        <div className="ml-5 h-[90%] mt-2">
-                            <h1 className="text-xl font-bold">O Diário de Anne Frank </h1>
+                    <div className="flex bg-white w-[75%] h-4/12 mb-5 rounded-md">
+                        <img src="./src/img/AnneFrankRed.png" className="w-45 h-70 flex self-center ml-4 mt-4 mb-4" alt="" />
+                        <div className="ml-5 mt-4">
+                            <h1 className="text-xl font-bold mb-2">O Diário de Anne Frank </h1>
                             <p>Autores: Anne Frank</p>
                             <p>Edição:--</p>
                             <p>Editora: Record</p>
@@ -76,7 +76,7 @@ function Consulta() {
 
                             <button
                                 onClick={() => setVerMais2(!verMais2)}
-                                className="text-black underline font-bold mt-2"
+                                className="text-black underline font-bold mt-2 cursor-pointer"
                             >
                                 {verMais2 ? "Ver menos" : "Ver mais"}
                             </button>
@@ -91,4 +91,4 @@ function Consulta() {
         </div>
     )
 }
-export default Consulta 
+export default Consulta
