14cbb625ab5353497c7888c6bbdc8fc97d813ebb
luanabrizola <130062674+luanabrizola@users.noreply.github.com>
2025-06-06 10:51:53 -0300

    Revert "model controller"
    This reverts commit 5f5f5ac80db304e52c9947cb193ea6c3d6a481f4.

--- backend/db.js
+++
@@ -1,3 +0,0 @@
-import postgres from "postgres";
-
-export const sql = postgres("postgresql://neondb_owner:npg_c72ZkbUVGoeq@ep-withered-dew-a4hq0ydi-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require");
--- backend/routes/usuarioRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-import usuarioController from '../controllers/usuarioController.js';
-
-
-router.post('/usuario', usuarioController.criar); 
-router.get('/usuario', usuarioController.mostrar);
-
-module.exports = router;
